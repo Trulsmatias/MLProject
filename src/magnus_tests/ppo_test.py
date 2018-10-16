7d32c44ceee2ca15041c8198c5e9b9769d316adf
Magnus Conrad Hyll <magnus@hyll.no>
2018-10-16 13:18:27 +0200

    Magnus tests & updated requirements.txt

--- src/magnus_tests/ppo_test.py
+++ src/magnus_tests/ppo_test.py
@@ -1,27 +1,28 @@
 import gym_super_mario_bros
 import numpy as np
+import matplotlib.pyplot as plt
 from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
+from tensorforce.agents import PPOAgent
 from movements import basic_movements
 
 
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = BinarySpaceToDiscreteSpaceEnv(env, basic_movements)
 
-'''
 agent = PPOAgent(
-    states=dict(type='float', shape=(240, 256, 3)),
-    actions=dict(type='int', num_actions=len(basic_movements)),
+    states=dict(type='float', shape=(10,)),
+    actions=dict(type='int', num_actions=2),
     network=[
+        dict(type='flatten'),
         dict(type='dense', size=64),
         dict(type='dense', size=64)
     ],
-    batching_capacity=1000,
+    batching_capacity=10,
     step_optimizer=dict(
         type='adam',
         learning_rate=1e-4
     )
 )
-'''
 
 done = True
 state = np.empty((240, 256, 3))
@@ -29,9 +30,14 @@ state = np.empty((240, 256, 3))
 for step in range(5000):
     if done:
         state = env.reset()
+        #plt.imshow(state[:, :, 2], cmap='gray')
+        #plt.show()
 
-    # action = agent.act(state)
-    action = env.action_space.sample()
+    state = np.zeros((10,))
+    agent.states
+    action = agent.act(state)
+    print(action)
+    # action = env.action_space.sample()
     state, reward, done, info = env.step(action)
 
     # Train the agent model
