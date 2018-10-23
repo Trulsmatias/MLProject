aebe660c277e988ab34109e24a890a7e7bef9660
Magnus Conrad Hyll <magnus@hyll.no>
2018-10-23 17:07:15 +0200

    More evolution code

--- src/evolution.py
+++ src/evolution.py
@@ -2,16 +2,8 @@ from generations import Generation, Individual
 import numpy as np
 
 
-def selection(generation):
-    """
-    Select n fittest individuals from the generation.
-    :param generation:
-    :return:
-    """
-    return None
-
-
-def roulette_wheel_selection(generation: Generation, num_select: int):
+# TODO: Input a list of Individuals instead of the Generation object? In accordance with reproduce() and mutate()
+def roulette_wheel_selection(generation, num_select):
     """
     Performs roulette wheel selection on individuals in the generation.
     :param generation:
@@ -28,33 +20,66 @@ def roulette_wheel_selection(generation: Generation, num_select: int):
 
 
 def make_child(parents):
+    """
+    Make a single child from a list of parents.
+    :param parents: a list of parent which will make a child
+    :return: the child
+    """
+    # TODO: Cross-breed parents, now it only selects a random parent.
+    # This is where cross-breeding of weights and biases of the parents' NNs should happen.
+    # There's prob many ways to do this, maybe make several different versions
     return np.random.choice(parents)
 
 
-def reproduce(individuals, num_parents_per_child, num_children_per_family):
+def reproduce(parents, num_parents_per_child, num_children_total):
     """
-
-    :return:
+    Creates a number of children by reproduction.
+    :param parents: the parents, aka. the fittest individuals after selection
+    :param num_parents_per_child:
+    :param num_children_total:
+    :return: children of the next generation
     """
+    children = []
+    for i in range(0, len(parents), num_parents_per_child):
+        family_parents = parents[i:i+num_parents_per_child]
+        num_children_per_family = num_children_total // (len(parents) // num_parents_per_child)
+        for j in range(num_children_per_family):  # TODO: fix this monstrosity
+            children.append(make_child(family_parents))
 
-    return
+    return children
 
 
-def mutate():
-    pass
+def mutate(children):
+    """
+    Mutates individuals (children).
+    :param children: a list of Individuals (the children) to mutate
+    :return: a list of mutated children
+    """
+    # TODO: Mutate the children (add randomness to weights/biases in the NNs).
+    return children
 
 
 if __name__ == '__main__':
-    individuals = [
-        Individual(1, 849),
-        Individual(2, 849),
-        Individual(3, 849)
-    ]
-
     np.random.seed(42)
-    for i in individuals:
-        i.fitness = np.random.random()
+
+    individuals = []
+    for i in range(12):
+        chromosomes = np.random.random()
+        individual = Individual(i + 1, chromosomes)
+        individuals.append(individual)
 
     gen = Generation(1, individuals)
-    print(gen.individuals)
-    print(roulette_wheel_selection(gen, 1))
+
+    for i in range(10):
+        for individual in gen.individuals:
+            individual.fitness = np.random.random()
+
+        best = roulette_wheel_selection(gen, 4)
+        children = reproduce(best, num_parents_per_child=2, num_children_total=12)
+        children = mutate(children)
+
+        print(gen)
+        gen = Generation(gen.num + 1, children)
+
+        # print(gen.individuals)
+        # print(roulette_wheel_selection(gen, 1))
--- src/generations.py
+++ src/generations.py
@@ -4,14 +4,17 @@ class Individual:
         self.fitness = 0
         self.chromosomes = chromosomes
 
-    def __str__(self):
-        return 'Individual(id={}, fitness={}, chromosomes={})'.format(self.id, self.fitness, self.chromosomes)
-
     def __repr__(self):
-        return self.__str__()
+        return 'Individual(id={}, fitness={}, chromosomes={})'.format(self.id, self.fitness, self.chromosomes)
 
 
 class Generation:
     def __init__(self, num, individuals):
         self.num = num
         self.individuals = individuals
+
+    def __repr__(self):
+        individuals_str = ''
+        if len(self.individuals) > 0:
+            individuals_str = '\n    ' + '\n    '.join([str(i) for i in self.individuals]) + '\n'
+        return 'Generaiton(num={}, children={})'.format(self.num, individuals_str)
